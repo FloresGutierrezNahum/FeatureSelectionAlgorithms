cc447cac971bbe7d460e386cac10befcfe5c5c68
NahumFG <36179329+FloresGutierrezNahum@users.noreply.github.com>
2020-07-04 21:57:14 -0500

    ReliefF terminado

--- 01. FuncionesReliefF/A-ReliefF.py
+++ 01. FuncionesReliefF/A-ReliefF.py
@@ -1,11 +1,22 @@
+#https://medium.com/@yashdagli98/feature-selection-using-relief-algorithms-with-python-example-3c2006e18f83
+
 from ReliefF import ReliefF
 import numpy as np
 from sklearn import datasets
 import pandas as pd
 
 #example of 2 class problem
-data = np.array([[9,2,2],[5,1,0],[9,3,2],[8,3,1],[6,0,0]])
-target = np.array([0,0,1,1,1])
+data = np.array([[9,2,2],
+                 [5,1,0],
+                 [9,3,2],
+                 [8,3,1],
+                 [6,0,0]]
+                 )
+target = np.array([0,
+                   0,
+                   1,
+                   1,
+                   1])
 
 fs = ReliefF(n_neighbors=1, n_features_to_keep=2)
 X_train = fs.fit_transform(data, target)
@@ -17,6 +28,8 @@ print("(No. of tuples, No. of Columns before ReliefF) : "+str(data.shape)+
 
 #example of multi class problem
 iris = datasets.load_iris()
+#print(iris)
+
 X = iris.data
 Y = iris.target
 
