91a68a7b5dc70461453cf0fc736b497321eb8c72
NahumFG <36179329+FloresGutierrezNahum@users.noreply.github.com>
2020-07-05 00:36:39 -0500

    LDA

---
+++ 07. LDA/A-LDA.py
@@ -0,0 +1,12 @@
+#https://scikit-learn.org/stable/modules/generated/sklearn.discriminant_analysis.LinearDiscriminantAnalysis.html
+
+import numpy as np
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+
+X = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
+y = np.array([1, 1, 1, 2, 2, 2])
+
+clf = LinearDiscriminantAnalysis()
+clf.fit(X, y)
+
+print(clf.predict([[-0.8, -1]]))
